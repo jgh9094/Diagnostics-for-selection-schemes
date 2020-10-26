de506f4f6b90eb6066f2e50bd137bfa4ceb4f847
jgh9094 <jgh9094@gmail.com>
2020-10-26 19:00:31 -0400

    added new files for refactoring

---
+++ Experiment/source/diagnostic.h
@@ -0,0 +1,24 @@
+/// These are the digital organisms we are using for the diagnostic experiments
+
+#ifndef DIA_PRO_H
+#define DIA_PRO_H
+
+///< standard headers
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <string>
+#include <utility>
+
+///< empirical headers
+#include "base/vector.h"
+
+class Diagnostic
+{
+  public:
+
+    Diagnostic() {;}
+
+};
+#endif
