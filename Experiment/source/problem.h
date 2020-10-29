67b58fa899636cf6eba3b6b4c63ca91fd37e13be
jgh9094 <jgh9094@gmail.com>
2020-10-28 23:27:58 -0400

    added exploitation and structured eploitation, tests needed

--- Experiment/source/problem.h
+++ Experiment/source/problem.h
@@ -39,15 +39,125 @@ class Diagnostic
 
     // exploration function
     score_t Exploration(const genome_t & g);
-    // exploitation function
+
+    /**
+     * Exploration function. All positions in genome evaluated independently.
+     * This means that we just return the same vector, in principle.
+     * Assuming that genes in genome cannot go over the target.
+     *
+     * @param g Genome from organism being evaluated.
+     * @return score vector that is calculated from 'g'.
+     */
     score_t Exploitation(const genome_t & g);
+
     // ecological niche function
-    score_t Exploration(const genome_t & g);
-    // structured exploration function
-    score_t Exploration(const genome_t & g);
+    score_t EcologyNiche(const genome_t & g);
+
+    /**
+     * Structured Exploitation function.
+     * All genes in genome expect to follow accending order for evaluation.
+     * If accending order is broken, max error assigned to every position after broken.
+     * Assuming that genes in genome cannot go over the target.
+     *
+     * @param g Genome from organism being evaluated.
+     * @param max_error This value is the maximum error assigned when structure broken
+     * @return score vector that is calculated from 'g'.
+     */
+    score_t StructExploitation(const genome_t & g, double max_error);
 
   private:
     // variable that holds target vector
     target_t target;
 };
+
+
+
+// exploration function
+Diagnostic::score_t Diagnostic::Exploration(const genome_t & g)
+{
+  // quick checks
+  emp_assert(g.size() == target.size(), g.size());
+
+  // intialize vector with size g
+  score_t score(g.size());
+
+
+
+
+  return score;
+}
+
+
+Diagnostic::score_t Diagnostic::Exploitation(const genome_t & g)
+{
+  // quick checks
+  emp_assert(g.size() == target.size(), g.size());
+
+  // intialize vector with size g
+  score_t score(g.size());
+
+  for(size_t i = 0; i < g.size(); ++i)
+  {
+    score[i] = g[i];
+  }
+
+  return score;
+}
+
+
+// ecological niche function
+Diagnostic::score_t Diagnostic::EcologyNiche(const genome_t & g)
+{
+  score_t score;
+
+
+  return score;
+}
+
+Diagnostic::score_t Diagnostic::StructExploitation(const genome_t & g, double max_error)
+{
+  // quick checks
+  emp_assert(g.size() == target.size(), g.size());
+
+  // intialize vector with size g
+  score_t score(g.size());
+
+  // loop through vector and check if structure holds
+  size_t error = g.size();
+  for(size_t i = 0; i < g.size()-1; ++i)
+  {
+    // debugging
+    std::cerr << "(" << i <<"," << i+1 <<"), ";
+
+    // if correct order
+    if(g[i] <= g[i+1]) {score[i] = g[i];}
+
+    else
+    {
+      score[i] = max_error;
+      error = i;
+      break;
+    }
+  }
+
+  // check if we broke structure
+  if(error < g.size())
+  {
+    // everything after broken structure set to max error
+    for(size_t i = error + 1; i < g.size(); ++i) {score[i] = max_error;}
+  }
+
+  else
+  {
+    score[g.size()-1] = g[g.size()-1];
+  }
+
+
+  return score;
+}
+
+
+
+
+
 #endif
