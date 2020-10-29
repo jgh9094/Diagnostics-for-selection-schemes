99ee1eebe68fabeceffd9360d458714a1417e8d7
jgh9094 <jgh9094@gmail.com>
2020-10-28 22:27:41 -0400

    added skeleton for diagnostic functions of interest

--- Experiment/source/problem.h
+++ Experiment/source/problem.h
@@ -17,8 +17,37 @@
 class Diagnostic
 {
   public:
+    //typename for target vector
+    using target_t = emp::vector<double>;
+    using score_t = emp::vector<double>;
+    using genome_t = emp::vector<double>;
 
-    Diagnostic() {;}
+  public:
 
+    Diagnostic(target_t & t_)
+    {
+      target.resize(t_.size());
+      std::copy(t_.begin(), t_.end(), target.begin());
+    }
+
+    // getters
+    target_t GetTarget() const {return target;}
+    // const target_t & GetTarget() const {return target;}
+
+    //setters
+    void SetTarget(target_t & t) {target.clear(); target.resize(t.size()); std::copy(t.begin(), t.end(), target.begin());}
+
+    // exploration function
+    score_t Exploration(const genome_t & g);
+    // exploitation function
+    score_t Exploitation(const genome_t & g);
+    // ecological niche function
+    score_t Exploration(const genome_t & g);
+    // structured exploration function
+    score_t Exploration(const genome_t & g);
+
+  private:
+    // variable that holds target vector
+    target_t target;
 };
 #endif
