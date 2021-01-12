719b8afda96a845a7e6dd658a62e0b92b84a4217
jgh9094 <jgh9094@gmail.com>
2021-01-12 11:51:58 -0500

    initial implementation of contradictory ecology

--- Experiment/source/problem.h
+++ Experiment/source/problem.h
@@ -1,15 +1,17 @@
-/// These are the digital organisms we are using for the diagnostic experiments
+/// Set of diagnostic problems being used in research project
+
+// TODO
+// Exploration []
+// Exploitation []
+// ContraEcology []
+// StructExploitation [X]
+
 
 #ifndef DIA_PRO_H
 #define DIA_PRO_H
 
 ///< standard headers
 #include <algorithm>
-#include <cmath>
-#include <fstream>
-#include <functional>
-#include <string>
-#include <utility>
 
 ///< empirical headers
 #include "base/vector.h"
@@ -39,7 +41,17 @@ class Diagnostic
     //setters
     void SetTarget(target_t & t) {target.clear(); target.resize(t.size()); std::copy(t.begin(), t.end(), target.begin());}
 
-    // exploration function
+    /**
+     * Exploitation function.
+     * All genes in genome expected to follow descending order for evaluation.
+     * While in decending order, fitness vector score at positon 'i' is gene value at position 'i'.
+     * If descending order is broken, max error assigned at position descending order is broken, and after.
+     * Assuming that genes in genome cannot go over the target vector per position in genome.
+     *
+     * @param g Genome from organism being evaluated.
+     * @param max_error This value is the maximum error assigned when structure broken
+     * @return score vector that is calculated from 'g'.
+     */
     score_t Exploration(const genome_t & g);
 
     /**
@@ -52,14 +64,28 @@ class Diagnostic
      */
     score_t Exploitation(const genome_t & g);
 
-    // ecological niche function
-    score_t EcologyNiche(const genome_t & g);
+    /**
+     * Contradicting Ecology function
+     *
+     * All genes are associated with a single type of resource.
+     * Solutions are pressured to optimize one maximaly, while keeping others close to 0.
+     * To find calculate fitness vector, first we find the maximum gene value i*.
+     * Once i* is found, fitness vector score at position i is i* minus gene value at position i.
+     * Fitness vector score where i* is found is just i*.
+     *
+     * @param g Genome from organism being evaluated.
+     * @param max_error This value is the maximum error assigned when structure broken
+     * @return score vector that is calculated from 'g'.
+     */
+    score_t ContraEcology(const genome_t & g);
 
     /**
      * Structured Exploitation function.
-     * All genes in genome expect to follow descending order for evaluation.
-     * If descending order is broken, max error assigned to every position after broken.
-     * Assuming that genes in genome cannot go over the target.
+     *
+     * All genes in genome expected to follow descending order for evaluation.
+     * While in decending order, fitness vector score at positon 'i' is gene value at position 'i'.
+     * If descending order is broken, max error assigned at position descending order is broken in fitness vector, and after.
+     * Assuming that genes in genome cannot go over the target vector per position in genome.
      *
      * @param g Genome from organism being evaluated.
      * @param max_error This value is the maximum error assigned when structure broken
@@ -89,7 +115,6 @@ Diagnostic::score_t Diagnostic::Exploration(const genome_t & g)
   return score;
 }
 
-
 Diagnostic::score_t Diagnostic::Exploitation(const genome_t & g)
 {
   // quick checks
@@ -107,11 +132,22 @@ Diagnostic::score_t Diagnostic::Exploitation(const genome_t & g)
 }
 
 
-// ecological niche function
-Diagnostic::score_t Diagnostic::EcologyNiche(const genome_t & g)
+Diagnostic::score_t Diagnostic::ContraEcology(const genome_t & g)
 {
-  score_t score;
+  // intialize score vector
+  score_t score(g.size());
 
+  // find max value position
+  size_t skip = (std::max_element(g.begin(), g.end()) - g.begin());
+
+  // set all score vector values except where max value is
+  for(size_t i = 0; i < score.size(); ++i)
+  {
+    if(i == skip) {continue;}
+    score[i] = g[skip] - g[i];
+  }
+
+  score[skip] = g[skip];
 
   return score;
 }
@@ -121,12 +157,13 @@ Diagnostic::score_t Diagnostic::StructExploitation(const genome_t & g, double ma
   // intialize vector with size g
   score_t score(g.size());
 
-  // using stl algorithm library
+  // find where order breaks
   auto it = std::is_sorted_until(g.begin(), g.end(), std::greater_equal<>());
 
-  // if sorted
+  // if sorted, return same vector
   if(it == g.end()) {std::copy(g.begin(), g.end(), score.begin());}
 
+  // else fill in appropiately
   else
   {
     size_t cutoff = (it - g.begin());
