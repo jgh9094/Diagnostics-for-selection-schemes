7239c40687ac2c9c35e19ac5e308ca64948d560d
jgh9094 <jgh9094@gmail.com>
2021-01-13 16:25:24 -0500

    finished initial implementation of org (no test/run)

--- Experiment/source/org.h
+++ Experiment/source/org.h
@@ -1,7 +1,7 @@
 /// digital organisms (real number vectors) for the diagnostic experiments
 
-#ifndef DIA_ORG_H
-#define DIA_ORG_H
+#ifndef ORG_H
+#define ORG_H
 
 ///< standard headers
 #include <algorithm>
@@ -9,8 +9,9 @@
 ///< empirical headers
 #include "base/vector.h"
 
-///< score we start from
-constexpr double START = 0.0;
+///< coordiante we start from
+constexpr double START_DB = 0.0;
+constexpr size_t START_ST = 0;
 
 
 class Org
@@ -26,14 +27,20 @@ class Org
 
   public:
     // for initial population
-    Org(size_t _m) : M(_m)
+    Org(size_t _m)
     {
-      genome.resize(_m, START);
+      // make sure nun weird is happening
+      emp_assert(genome.size() == 0); emp_assert(M == 0);
+      M = _m;
+      genome.resize(_m, START_DB);
     }
 
     // every org after starting generation
-    Org(genome_t _g) : M(_g.size())
+    Org(genome_t _g)
     {
+      // make sure we aren't seeing anything weird
+      emp_assert(genome.size() == 0); emp_assert(M == 0);
+      M = _g.size();
       genome.resize(M);
       std::copy(_g.begin(), _g.end(), genome.begin());
     }
@@ -48,25 +55,60 @@ class Org
     ///< getters
 
     // const + reference to
-    const genome_t & GetGenome() const {return genome;}
-    const score_t & GetScore() const {return score;}
-    const optimal_t & GetOptimal() const {return optimal;}
+    const genome_t & GetGenome() const {emp_assert(0 < genome.size()); return genome;}
+    const score_t & GetScore() const {emp_assert(scored); return score;}
+    const optimal_t & GetOptimal() const {emp_assert(opti); return optimal;}
     // reference to
-    genome_t & GetGenome() {return genome;}
-    score_t & GetScore() {return score;}
-    optimal_t & GetOptimal() {return optimal;}
+    genome_t & GetGenome() {emp_assert(0 < genome.size()); return genome;}
+    score_t & GetScore() {emp_assert(scored); return score;}
+    optimal_t & GetOptimal() {emp_assert(opti); return optimal;}
     // get const aggregate fitness
     double GetAggregate() const {emp_assert(aggregated); return agg_score;}
     // get clone bool
-    const bool & GetClone() const {return clone;}
+    const bool & GetClone() const {emp_assert(0 < genome.size()); return clone;}
     // get optimal
     const size_t & GetCount() const {emp_assert(counted); return count;}
+    // get gene count
+    size_t & GetM() {emp_assert(0 < M); return M;}
 
 
     ///< setters
 
-    // set score vector (recieved from problem.h in world.h)
-    void SetScore(const score_t & s_) {score.resize(s_.size()); std::copy(s_.begin(), s_.end(), score.begin());}
+    // set score vector (recieved from problem.h in world.h or inherited from parent)
+    void SetScore(const score_t & s_)
+    {
+      // make sure that score vector hasn't been set before.
+      emp_assert(!scored); emp_assert(s_.size() == M); emp_assert(score.size() == 0); emp_assert(0 < M);
+      scored = true;
+      score.resize(s_.size());
+      std::copy(s_.begin(), s_.end(), score.begin());
+    }
+
+    // set the optimal gene vector (recieved from problem.h in world.h or inherited from parent)
+    void SetOptimal(const optimal_t & o_)
+    {
+      // make sure that optimal gene vector hasn't been set before.
+      emp_assert(!opti); emp_assert(o_.size() == M); emp_assert(optimal.size() == 0); emp_assert(0 < M);
+      opti = true;
+      optimal.resize(o_.size());
+      std::copy(o_.begin(), o_.end(), optimal.begin());
+    }
+
+    // set the optimal gene count (called from world.h or inherited from parent)
+    void SetCount(size_t c_)
+    {
+      emp_assert(!counted); emp_assert(0 < M);
+      counted = true;
+      count = c_;
+    }
+
+    // set the aggregated score (called from world.h or inherited from parent)
+    void SetAggregate(double a_)
+    {
+      emp_assert(!aggregated); emp_assert(0 < M);
+      aggregated = true;
+      agg_score = a_;
+    }
 
 
     ///< functions to calculate scores and related data
@@ -109,64 +151,114 @@ class Org
      *
      * Will pass all info from parent to offspring solution.
      * Function executes if offpsring is an clone
+     *
+     * @param s score vector recived
+     * @param o optimal gene vector recieved
+     * @param c optimal gene count recieved
+     * @param a aggregate score recieved
+     *
     */
-    void Inherit();
+    void Inherit(const score_t & s, const optimal_t & o, size_t c, double a);
 
     /**
      * Me Clone function:
      *
      * Will set the clone variable to true.
     */
-    void MeClone() {clone = true;}
+    void MeClone() {emp_assert(0 < M); emp_assert(!clone); clone = true;}
 
   private:
     // organism genome vector
     genome_t genome;
+
     // organism score vector
     score_t score;
+    // score vector set?
+    bool scored = false;
+
     // organims gene optimal vector
     optimal_t optimal;
     // gene optimal vector calculated?
     bool opti = false;
+
     // optimal gene count
     size_t count = 0;
     // gene optimal vector counted?
     bool counted = false;
+
     // aggregate score
     double agg_score = 0.0;
     // aggregate calculate?
     bool aggregated = false;
+
     // Number of genes in genome
-    size_t M;
+    size_t M = 0;
+
     // Are we a clone?
     bool clone = false;
-
 };
+
+///< functions to calculate scores and related data
 
 double Org::AggregateScore()
 {
   //quick checks
-  emp_assert(!aggregated);
+  emp_assert(!aggregated); emp_assert(0 < M);
   emp_assert(score.size() == M, score.size());
 
-  // calculate the aggregate score
-  agg_score = std::accumulate(score.begin(), score.end(), START);
-  aggregated = true;
+  // calculate the aggregate score and set it
+  SetAggregate(std::accumulate(score.begin(), score.end(), START_DB));
 
   return agg_score;
 }
-
 size_t Org::CountOptimized()
 {
   //quick checks
-  emp_assert(!counted);
+  emp_assert(!counted); emp_assert(0 < M);
   emp_assert(optimal.size() == M, optimal.size());
 
-  // calculate total optimal genes
-  count = std::accumulate(optimal.begin(), optimal.end(), START);
-  counted = true;
+  // calculate total optimal genes and set it
+  SetCount(std::accumulate(optimal.begin(), optimal.end(), START_ST));
 
   return count;
+}
+
+///< functions related to the birth of an organism
+
+void Org::Reset()
+{
+  // quick checks
+  emp_assert(0 < M); emp_assert(0 < genome.size());
+
+  // reset score vector stuff
+  score.clear();
+  scored = false;
+
+  // reset optimal gene vector stuff
+  optimal.clear();
+  opti = false;
+
+  // reset optimal gene count stuff
+  count = 0;
+  counted = false;
+
+  // reset aggregate score stuff
+  agg_score = 0.0;
+  aggregated = false;
+
+  // reset clone var
+  clone = false;
+}
+void Org::Inherit(const score_t & s, const optimal_t & o, size_t c, double a)
+{
+  // quick checks
+  emp_assert(0 < M); emp_assert(0 < genome.size()); emp_assert(!clone);
+
+  // copy everything into offspring solution
+  SetScore(s);
+  SetOptimal(o);
+  SetCount(c);
+  SetAggregate(a);
 }
 
 #endif
