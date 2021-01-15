a1e5d5f9598c05ad32e6441140815a71b8a91de8
jgh9094 <jgh9094@gmail.com>
2021-01-15 12:30:01 -0500

    skeleton for research project

--- Experiment/source/selection.h
+++ Experiment/source/selection.h
@@ -1,15 +1,11 @@
-/// These are the digital organisms we are using for the diagnostic experiments
+/// These are the selection schemes we are using for this project
+/// Will be broken up according to my thoughts on how $$ components work
 
-#ifndef DIA_SEL_H
-#define DIA_SEL_H
+#ifndef SEL_H
+#define SEL_H
 
 ///< standard headers
 #include <algorithm>
-#include <cmath>
-#include <fstream>
-#include <functional>
-#include <string>
-#include <utility>
 
 ///< empirical headers
 #include "base/vector.h"
@@ -17,8 +13,119 @@
 class Selection
 {
   public:
+    // vector type of parent ids selected
+    using parent_t = emp::vector<size_t>;
+    // vector type of org score
+    using score_t = emp::vector<double>;
+    // matrix type of org with multiple scorea
+    using matrix_t = emp::vector<emp::vector<double>>;
+    // vector holding population genomes
+    using genome_t = emp::vector<emp::vector<double>>;
+    // vector holding population groupings
+    using group_t = emp::vector<emp::vector<size_t>>;
+
+  public:
 
     Selection() {;}
+
+    ///< population structure
+
+    /**
+     * K-Nearest Neighbor Structure:
+     *
+     * This function will return a vector of vectors, where
+     * each individual vector corresponds to that solutions nearest neighbor
+     *
+     * In the event of ties, a solution will be selected at random.
+     *
+     * @param score Vector containing all solution scores.
+     *
+     * @return Vector with parent id's that are selected.
+     */
+    group_t KNearestN(const score_t & score);
+
+
+    ///< fitness transformation
+
+    /**
+     * Fitness Sharing Transformation:
+     *
+     * Fitness transformation function inspired from:
+     * B. Sareni and L. Krahenbuhl, “Fitness sharing and niching methods revisited,”
+     * in IEEE Transactions on Evolutionary Computation, vol. 2, no. 3, pp. 97-106, Sept. 1998.
+     *
+     *
+     * @param genome Vector containing all genomes.
+     * @param score Vector containing all solution scores.
+     * @param alph Shape of sharing function.
+     * @param sig Similarity threshold.
+     *
+     * @return Vector with parent id's that are selected.
+     */
+    score_t FitnessSharing(const genome_t & genome, const score_t & score, const double alph, const double sig);
+
+    /**
+     * Novelty Fitness Transformation:
+     *
+     * This function will transform original fitness values as novelty scores.
+     * Each score has its associated neighbors (2nd argument).
+     *
+     *
+     * @param score Vector containing all solution scores.
+     * @param neigh Vector containing neighboring positions in score (per solution)
+     *
+     * @return Vector with transformed scores.
+     */
+    score_t Novelty(const scores_t & score, const group_t & neigh);
+
+
+    ///< selector functions
+
+    /**
+     * (μ,λ) Elite Selector:
+     *
+     * This function finds the top 'm' performing solutions.
+     * Once the top 'm' solutions are found, each will be placed (l/m) times in the parent list.
+     *
+     * @param top Number of top performing solutions to pick.
+     * @param pops Population size.
+     * @param score Vector holding the population score.
+     *
+     * @return Vector with parent id's that are selected.
+     */
+    parent_t MLElite(const size_t mu, const size_t lambda, const score_t & score);
+
+    /**
+     * Tournament Selector:
+     *
+     * This function holds tournaments for 't' solutions randomly picked from the population.
+     * In the event of ties, a solution will be selected randomly from all solutions that tie.
+     *
+     * @param t Tournament size.
+     * @param score Vector holding the population score.
+     *
+     * @return Parent id of solution that won the tournament.
+     */
+
+    size_t Tournament(const size_t t, const score_t & score);
+
+    /**
+     * Drift Selector:
+     *
+     * This function will return a random solution as a parent.
+     *
+     *
+     * @param score Vector containing all solution scores.
+     *
+     * @return Parent id that is selected.
+     */
+    size_t Drift(const score_t & score);
+
+
+    ///< helper functions
+
+
+  private:
 
 };
 #endif
