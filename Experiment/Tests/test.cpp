8f49e5ae6a8969b8211b14a639d5db0d2ade5226
jgh9094 <jgh9094@gmail.com>
2021-02-02 15:46:15 -0500

    added tests for structured exploitation

--- Experiment/Tests/test.cpp
+++ Experiment/Tests/test.cpp
@@ -11,26 +11,29 @@
 #include <algorithm>
 #include <string>
 
-// To run: clang++ -std=c++17 -I ../../../Empirical/source/ test.cpp -o test; ./test -l -t -s
+// To run: clang++ -std=c++17 -I ../../../Empirical/source/ test.cpp -o test; ./test
 
 void PrintVec(const emp::vector<double> &v, const std::string s)
 {
   std::cerr << s << ": ";
-  for(auto x : v) {std::cerr << x << ",";}
+  for(auto x : v) {std::cerr << (double) x << ",";}
   std::cerr << std::endl;
 }
 
-TEST_CASE( "Problem class initialization", "[initialization]" )
+TEST_CASE("Problem class initialization", "[initialization]")
 {
   // first round of tests
   emp::vector<double> tar(1.0,100);
   emp::vector<double> tar_d(1.0,100);
-  Diagnostic diagnostic(tar);
+  const double credit = 0.0;
+  Diagnostic diagnostic(tar, credit);
 
   // make sure we are saving the vector and can find the same one
   REQUIRE_THAT(diagnostic.GetTarget(), Catch::Matchers::Equals(tar));
   // make sure we can get the same vector contents with differnt vars
   REQUIRE_THAT(diagnostic.GetTarget(), Catch::Matchers::Equals(tar_d));
+  // make sure that credit is set
+  REQUIRE(credit == diagnostic.GetCredit());
 
   // switch the target vector
   emp::vector<double> targ(2.0, 100);
@@ -43,56 +46,62 @@ TEST_CASE( "Problem class initialization", "[initialization]" )
   REQUIRE_THAT(diagnostic.GetTarget(), Catch::Matchers::Equals(targ1));
 }
 
-TEST_CASE( "Problem class structured exploitation function", "[struct-exploit]")
+TEST_CASE("Problem class structured exploitation function", "[struct-exploit]")
 {
   // set up our genome and diagnostic to score with structured exploitation
-  const size_t size = 10;
-  const double error = -1.0;
+  const size_t size = 10; const double cred = 0.0; const double max = 100.0;
   emp::vector<double> g(size);
   emp::vector<double> correct(size);
 
+  // create decending vector
+  // <10, 9,..., 1>
+  for(size_t i = size; i > 0; --i) {g[size-i] = (double) i; correct[size-i] = double(i);}
 
-  /* Making sure that a decending vector is calculated correctly */
-  for(size_t i = size; i > 0; --i) {g[size-i] = i;}
   // create diagnostic + test function
-  Diagnostic diag;
-  emp::vector<double> score = diag.StructExploitation(g, error);
-  // checking
+  Diagnostic diag(g, cred);
+  emp::vector<double> score = diag.StructExploitation(g);
+  // PrintVec(g, "g:"); PrintVec(score, "s:");
   REQUIRE_THAT(score, Catch::Matchers::Equals(g));
 
 
-  /* Making sure that a decending sorted vector is calculated correctly */
+  // Making sure that a decending sorted vector is calculated correctly
+  // <1, 2,..., 10>
   std::reverse(g.begin(), g.end());
-  score = diag.StructExploitation(g, error);
+  score = diag.StructExploitation(g);
   // create correct vector
-  correct[0] = g[0];
-  for(size_t i = 1; i < correct.size(); ++i) {correct[i] = error;}
-  // checking
+  correct = {1,cred,cred,cred,cred,cred,cred,cred,cred,cred};
+  // PrintVec(correct, "c"); PrintVec(g, "g");
   REQUIRE_THAT(score, Catch::Matchers::Equals(correct));
 
 
-  /* Checking accending vector last element out of order */
+  // Checking accending order when last element out of order
+  // <10, 9,...,10>
   std::reverse(g.begin(), g.end());
   g.back() = size;
-  score = diag.StructExploitation(g, error);
+  score = diag.StructExploitation(g);
   // create correct vector
-  correct.clear(); correct.resize(size);
-  std::copy(g.begin(), g.end(), correct.begin());
-  correct.back() = error;
-  // make sure we get the correct vector
+  correct = {10, 9, 8, 7, 6, 5, 4, 3, 2, cred};
+  // PrintVec(g, "g"); PrintVec(score, "s"); PrintVec(correct, "c");
   REQUIRE_THAT(score, Catch::Matchers::Equals(correct));
 
 
-  /* Checking accending vector middle & last element out of order */
+  // checking accending vector middle to end
+  // <10,9,8,7,6,10,4,3,2,1>
+  g.back() = 1;
   g[size/2] = size;
-  score = diag.StructExploitation(g, error);
+  score = diag.StructExploitation(g);
   // create correct vector
-  for(size_t i = size/2; i < correct.size(); ++i) {correct[i] = error;}
-  // make sure we get the correct vector
+  correct = {10, 9, 8, 7, 6, cred, cred, cred, cred, cred};
+  // PrintVec(g, "g"); PrintVec(score, "s"); PrintVec(correct, "c");
   REQUIRE_THAT(score, Catch::Matchers::Equals(correct));
 
-  // debug
-  // PrintVec(g, "g");
-  // PrintVec(correct, "c");
-  // PrintVec(score, "s");
+
+  // checking for accending vector middle to end
+  // <1,9,8,7,6,5,4,3,2,1>
+  g = {1,9,8,7,6,5,4,3,2,1};
+  score = diag.StructExploitation(g);
+  //create correct vector
+  correct = {1, cred, cred, cred, cred, cred, cred, cred, cred,cred};
+  // PrintVec(g, "g"); PrintVec(score, "s"); PrintVec(correct, "c");
+  REQUIRE_THAT(score, Catch::Matchers::Equals(correct));
 }
