234e671cbe3fb77cad80b018d9bb7cb4e91965f6
jgh9094 <jgh9094@gmail.com>
2021-03-12 14:04:48 -0500

    gens needs to be an int

--- DataTools/Checker/run-checker.py
+++ DataTools/Checker/run-checker.py
@@ -164,7 +164,7 @@ def CheckDir(dir, sel, dia, offs, obj, acc, gens):
             continue
 
         # make sure that the data.csv file did in fact finish all generations
-        if CountRows(DATA_DIR + 'data.csv') != gens:
+        if CountRows(DATA_DIR + 'data.csv') != int(gens) - 1:
             DAT_DNF.append(int(seed))
             continue
 
