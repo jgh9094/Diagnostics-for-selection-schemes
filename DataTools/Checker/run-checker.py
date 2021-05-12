cf21322944255cdd0b8899322ea037f844bdf7b6
jgh9094 <jgh9094@gmail.com>
2021-05-12 00:20:18 -0400

    removed checks for lexicase 10.0

--- DataTools/Checker/run-checker.py
+++ DataTools/Checker/run-checker.py
@@ -92,7 +92,7 @@ def SetSeeds(s):
     elif s == 3:
         return [x for x in range(1,401)]
     elif s == 4:
-        return [x for x in range(1,401)]
+        return [x for x in range(1,351)]
     else:
         sys.exit('SEEDS SELECTION UNKNOWN')
 
