a2995a95220159b94cd7c006611c4a279862b4a5
Jose <jgh9094@gmail.com>
2022-01-20 20:46:24 -0500

    hpcc scripts

---
+++ Hpcc/BOUNDS_0_1/Wrangling/TRT_100/wrangle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start the process
+echo "STARTING THE WRANGLING PROCESS"
+echo "CALLING THE SB FILES NOW..."
+echo ""
+
+echo "RUNNING: al-data.sb"
+sbatch al-data.sb
+
+echo "RUNNING: end-of-run.sb"
+sbatch end-of-run.sb
+
+echo "RUNNING: solution-fnd.sb"
+sbatch solution-fnd.sb
+
+echo "RUNNING: max-val.sb"
+sbatch max-val.sb
+
+echo ""
+echo "======================"
+echo "FINISHED RUNNING FILES"
+echo "======================"
