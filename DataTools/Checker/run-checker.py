cbc2824246cc87c93af8e55faa682e42b2a74729
Jose <jgh9094@gmail.com>
2022-06-27 11:10:02 -0400

    cleaned up and added data dictionary

--- DataTools/Checker/run-checker.py
+++ DataTools/Checker/run-checker.py
@@ -3,11 +3,14 @@
 # Will list all of the incomplete id's that need to finish running, per selection/diagnotic treatment
 #
 # Command Line Inputs
-#
-# Input 1: file directory where all the folders are located
-# Input 2: Selection scheme we are for
-# Input 3: Diagnostic we are looking for
-# Input 4: Experiment seed offset
+# data_directory: directory where data is located
+#      selection: selection scheme used
+#     diagnostic: diagnostic used
+#    seed_offset: seed offset (if any)
+#     objectives: dimensionality
+#       accuracy: satisfactory trait accuracy %
+#    generations: generations ran
+#      param_two: genotypic (0) or phenotypic (1) similarity for fitness sharing
 #
 # Output : list of seeds that need to be reran in terminal display
 #
@@ -22,127 +25,9 @@ import csv
 import sys
 import os
 
-# variables we are testing for each replicate range
-TR_LIST = ['1','2','4','8','16','32','64','128','256','512']
-TS_LIST = ['1','2','4','8','16','32','64','128','256','512']
-LX_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-FS_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-ND_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-NS_LIST = ['0','1','2','4','8','15','30','60']
-
-# seed experiements replicates range
-SMAX = 50
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetSelection(s,p):
-    # case by case
-    if s == 0:
-        return 'TRUNCATION'
-    elif s == 1:
-        return 'TOURNAMENT'
-    elif s == 2:
-        if p == '0':
-            return 'FITSHARING_G'
-        elif p == '1':
-            return 'FITSHARING_P'
-        else:
-            sys.exit("UNKNOWN SELECTION")
-    elif s == 3:
-        return 'LEXICASE'
-    elif s == 4:
-        return 'NONDOMINATEDSORTING'
-    elif s == 5:
-        return 'NOVELTY'
-    else:
-        sys.exit("UNKNOWN SELECTION")
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetDiagnostic(s):
-    # case by case
-    if s == 0:
-        return 'EXPLOITATION'
-    elif s == 1:
-        return 'STRUCTEXPLOITATION'
-    elif s == 2:
-        return 'STRONGECOLOGY'
-    elif s == 3:
-        return 'EXPLORATION'
-    elif s == 4:
-        return 'WEAKECOLOGY'
-    else:
-        sys.exit('UNKNOWN DIAGNOSTIC')
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetSelectionVar(s):
-    # case by case
-    if s == 0:
-        return 'TR'
-    elif s == 1:
-        return 'T'
-    elif s == 2:
-        return 'SIG'
-    elif s == 3:
-        return 'EPS'
-    elif s == 4:
-        return 'SIG'
-    elif s == 5:
-        return 'NOV'
-    else:
-        sys.exit("UNKNOWN SELECTION VAR")
-
-# return the correct amount of seed ran by experiment treatment
-def SetSeeds(s):
-    # case by case
-    if s == 0:
-        return [x for x in range(1,451)]
-    elif s == 1:
-        return [x for x in range(1,451)]
-    elif s == 2:
-        return [x for x in range(1,351)]
-    elif s == 3:
-        return [x for x in range(1,351)]
-    elif s == 4:
-        return [x for x in range(1,351)]
-    elif s == 5:
-        return [x for x in range(1,351)]
-    else:
-        sys.exit('SEEDS SELECTION UNKNOWN')
-
-# set the appropiate list of variables we are checking for
-def SetVarList(s):
-    # case by case
-    if s == 0:
-        return TR_LIST
-    elif s == 1:
-        return TS_LIST
-    elif s == 2:
-        return FS_LIST
-    elif s == 3:
-        return LX_LIST
-    elif s == 4:
-        return FS_LIST
-    elif s == 5:
-        return NS_LIST
-    else:
-        sys.exit("UNKNOWN VARIABLE LIST")
-
-# return extra parameter directory if needed
-def SetSecondParam(s, pt):
-    # case by case
-    if s == 0:
-        return ''
-    elif s == 1:
-        return ''
-    elif s == 2:
-        return ''
-    elif s == 3:
-        return ''
-    elif s == 4:
-        return ''
-    elif s == 5:
-        return ''
-    else:
-        sys.exit("UNKNOWN SELECTION")
+# file location for data-params.py file
+sys.path.insert(1, '../')
+import data_params
 
 # return the number of rows in a csv file
 def CountRows(file_name):
@@ -167,33 +52,31 @@ def CheckDir(dir, sel, dia, offs, obj, acc, gens, pt):
         print('Data dirctory exists=', dir)
     else:
         print('DOES NOT EXIST=', dir)
-        sys.exit('DATA DIRECTORY DOES NOT EXIST')
+        # sys.exit('DATA DIRECTORY DOES NOT EXIST')
 
     # check if selection scheme data folder exists
-    SEL_DIR = dir + SetSelection(sel,pt) + '/TRT_' + obj + '__ACC_' + acc + '__GEN_' + gens + '/'
+    SEL_DIR = dir + data_params.SetSelection(sel,pt) + '/TRT_' + obj + '__ACC_' + acc + '__GEN_' + gens + '/'
     if os.path.isdir(SEL_DIR):
         print('Selection scheme data folder exists', SEL_DIR)
     else:
         print('SELECTION DIRECTORY DOES NOT EXIST=', SEL_DIR)
-        sys.exit('SELECTION DATA DIRECTORY DOES NOT EXIST')
+        # sys.exit('SELECTION DATA DIRECTORY DOES NOT EXIST')
 
     # create seed data directories and check if exist
-    VLIST = SetVarList(sel)
+    VLIST = data_params.SetVarList(sel)
     DIR_DNE = []
     DAT_DNE = []
     DAT_DNF = []
 
-    SECOND_PARAM = SetSecondParam(sel, pt)
-
-    print('Full data Dir=', SEL_DIR + 'DIA_' + SetDiagnostic(dia) + '__' + SetSelectionVar(sel) + '_XXX' + '__SEED_XXX' + '/' + SECOND_PARAM)
+    print('Full data Dir=', SEL_DIR + 'DIA_' + data_params.SetDiagnostic(dia) + '__' + data_params.SetSelectionVar(sel) + '_XXX' + '__SEED_XXX' + '/')
     print('Now checking data replicates sub directories')
 
-    SEEDS = SetSeeds(sel)
+    SEEDS = data_params.SetSeeds(sel)
 
     for s in SEEDS:
         seed = str(s + offs)
-        var_val = str(VLIST[int((s-1)/SMAX)])
-        DATA_DIR =  SEL_DIR + 'DIA_' + SetDiagnostic(dia) + '__' + SetSelectionVar(sel) + '_' + var_val + '__SEED_' + seed + '/' + SECOND_PARAM
+        var_val = str(VLIST[int((s-1)/data_params.REPLICATES)])
+        DATA_DIR =  SEL_DIR + 'DIA_' + data_params.SetDiagnostic(dia) + '__' + data_params.SetSelectionVar(sel) + '_' + var_val + '__SEED_' + seed + '/'
         print('Sub directory:', DATA_DIR)
 
         # add full directory to missing list if not there
@@ -245,10 +128,10 @@ def main():
     data_dir = args.data_directory.strip()
     print('Data directory=',data_dir)
     selection = args.selection
-    print('Selection scheme=', SetSelection(selection,args.param_two))
+    print('Selection scheme=', data_params.SetSelection(selection,args.param_two))
     diagnostic = args.diagnostic
-    print('Diagnostic=',SetDiagnostic(diagnostic))
-    offset = args.seed_offset
+    print('Diagnostic=', data_params.SetDiagnostic(diagnostic))
+    offset = int(args.seed_offset)
     print('Offset=', offset)
     objectives = args.objectives
     print('Objectives=', objectives)
