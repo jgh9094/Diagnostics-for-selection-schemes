0bbb2bee538618e7b5050adcea34cc7b4a37ab19
Jose <jgh9094@gmail.com>
2022-06-28 19:13:18 -0400

    updated file with final data pulls

--- DataTools/Collector/al-data-collect.py
+++ DataTools/Collector/al-data-collect.py
@@ -11,176 +11,13 @@
 
 ######################## IMPORTS ########################
 import pandas as pd
-import numpy as np
 import argparse
-import math as mth
 import sys
 import os
 
-# variables we are testing for each replicate range
-TR_LIST = ['1','2','4','8','16','32','64','128','256']
-TS_LIST = ['1','2','4','8','16','32','64','128','256']
-LX_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-FS_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-ND_LIST = ['0.0','0.1','0.3','0.6','1.2','2.5','5.0']
-NS_LIST = ['0','1','2','4','8','15','30']
-
-# seed experiements replicates range
-REP_NUM = 50
-
-# columns we are interested in grabbing
-# pop level
-POP_FIT_AVG = 'pop_fit_avg'
-POP_FIT_MAX = 'pop_fit_max'
-POP_OPT_AVG = 'pop_opt_avg'
-POP_OPT_MAX = 'pop_opt_max'
-POP_UNI_OBJ = 'pop_uni_obj'
-POP_STR_AVG = 'pop_str_avg'
-POP_STR_MAX = 'pop_str_max'
-# elite solutions (max agg traits)
-ELE_AGG_PER = 'ele_agg_per'
-ELE_OPT_CNT = 'ele_opt_cnt'
-# # optimal solution (max opti trait count)
-# OPT_AGG_PER = 'opt_agg_per'
-# OPT_OBJ_CNT = 'opt_obj_cnt'
-# # streak solution (longest streak of non-zero values)
-# STR_AGG_PER = 'str_agg_per'
-# STR_OBJ_CNT = 'str_obj_cnt'
-# trait coverage
-UNI_STR_POS = 'uni_str_pos'
-# # pareto data
-PARETO_CNT = 'pareto_cnt'
-# # novelty data
-ARCHIVE_CNT = 'archive_cnt'
-PMIN = 'pmin'
-# ARC_ELITE = 'arc_elite'
-# ARC_OPTI = 'arc_opti'
-# ARC_STRK = 'arc_strk'
-ARC_ACTI_GENE = 'arc_acti_gene'
-OVERLAP = 'overlap'
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetSelection(s,p):
-    # case by case
-    if s == 0:
-        return 'TRUNCATION'
-    elif s == 1:
-        return 'TOURNAMENT'
-    elif s == 2:
-        if p == '0':
-            return 'FITSHARING_G'
-        elif p == '1':
-            return 'FITSHARING_P'
-        else:
-            sys.exit("UNKNOWN SELECTION")
-    elif s == 3:
-        return 'LEXICASE'
-    elif s == 4:
-        return 'NONDOMINATEDSORTING'
-    elif s == 5:
-        return 'NOVELTY'
-    else:
-        sys.exit("UNKNOWN SELECTION")
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetDiagnostic(s):
-    # case by case
-    if s == 0:
-        return 'EXPLOITATION'
-    elif s == 1:
-        return 'STRUCTEXPLOITATION'
-    elif s == 2:
-        return 'STRONGECOLOGY'
-    elif s == 3:
-        return 'EXPLORATION'
-    elif s == 4:
-        return 'WEAKECOLOGY'
-    else:
-        sys.exit('UNKNOWN DIAGNOSTIC')
-
-# return appropiate string dir name (based off run.sb file naming system)
-def SetSelectionVar(s):
-    # case by case
-    if s == 0:
-        return 'TR'
-    elif s == 1:
-        return 'T'
-    elif s == 2:
-        return 'SIG'
-    elif s == 3:
-        return 'EPS'
-    elif s == 4:
-        return 'SIG'
-    elif s == 5:
-        return 'NOV'
-    else:
-        sys.exit("UNKNOWN SELECTION VAR")
-
-# return the correct amount of seed ran by experiment treatment
-def SetSeeds(s):
-    # case by case
-    if s == 0 or s == 1:
-        seed = []
-        seed.append([x for x in range(1,51)])
-        seed.append([x for x in range(51,101)])
-        seed.append([x for x in range(101,151)])
-        seed.append([x for x in range(151,201)])
-        seed.append([x for x in range(201,251)])
-        seed.append([x for x in range(251,301)])
-        seed.append([x for x in range(301,351)])
-        seed.append([x for x in range(351,401)])
-        seed.append([x for x in range(401,451)])
-        return seed
-
-    elif s == 2 or s == 3 or s == 4 or s == 5:
-        seed = []
-        seed.append([x for x in range(1,51)])
-        seed.append([x for x in range(51,101)])
-        seed.append([x for x in range(101,151)])
-        seed.append([x for x in range(151,201)])
-        seed.append([x for x in range(201,251)])
-        seed.append([x for x in range(251,301)])
-        seed.append([x for x in range(301,351)])
-        return seed
-
-    else:
-        sys.exit('SEEDS SELECTION UNKNOWN')
-
-# set the appropiate list of variables we are checking for
-def SetVarList(s):
-    # case by case
-    if s == 0:
-        return TR_LIST
-    elif s == 1:
-        return TS_LIST
-    elif s == 2:
-        return FS_LIST
-    elif s == 3:
-        return LX_LIST
-    elif s == 4:
-        return FS_LIST
-    elif s == 5:
-        return NS_LIST
-    else:
-        sys.exit("UNKNOWN VARIABLE LIST")
-
-# return extra parameter directory if needed
-def SetSecondParam(s, pt):
-    # case by case
-    if s == 0:
-        return ''
-    elif s == 1:
-        return ''
-    elif s == 2:
-        return ''
-    elif s == 3:
-        return ''
-    elif s == 4:
-        return ''
-    elif s == 5:
-        return ''
-    else:
-        sys.exit("UNKNOWN SELECTION")
+# file location for data-params.py file
+sys.path.insert(1, '../')
+import data_params
 
 # loop through differnt files that exist
 def DirExplore(data, dump, sel, dia, offs, res, obj, acc, gens, pt):
@@ -195,21 +32,19 @@ def DirExplore(data, dump, sel, dia, offs, res, obj, acc, gens, pt):
         sys.exit('DATA DIRECTORY DOES NOT EXIST')
 
     # check that selection data folder exists
-    SEL_DIR = data + SetSelection(sel,pt) + '/TRT_' + obj + '__ACC_' + acc + '__GEN_' + gens + '/'
+    SEL_DIR = data + data_params.SetSelection(sel,pt) + '/TRT_' + obj + '__ACC_' + acc + '__GEN_' + gens + '/'
     if os.path.isdir(SEL_DIR) == False:
         print('SEL_DIR=', SEL_DIR)
         sys.exit('SELECTION DIRECTORY NOT FOUND')
 
     # Set vars that we need to loop through
-    VLIST = SetVarList(sel)
-    SEEDS = SetSeeds(sel)
+    VLIST = data_params.SetVarList(sel)
+    SEEDS = data_params.SetSeedSets(sel)
 
     # gens we are expecting
     GEN_LIST = [x for x in range(int(gens)+1) if x%res == 0]
     # data frame list for concatanation
     DF_LIST = []
-    # second parameter dir
-    SECOND_PARAM = SetSecondParam(sel, pt)
 
     # iterate through the sets of seeds
     for i in range(len(SEEDS)):
@@ -220,10 +55,10 @@ def DirExplore(data, dump, sel, dia, offs, res, obj, acc, gens, pt):
 
         # iterate through seeds to collect data
         for s in seeds:
-            it = int((s-1)/REP_NUM)
+            it = int((s-1)/data_params.REPLICATES)
             var_val = str(VLIST[it])
             seed = str(s + offs)
-            DATA_DIR =  SEL_DIR + 'DIA_' + SetDiagnostic(dia) + '__' + SetSelectionVar(sel) + '_' + var_val + '__SEED_' + seed + '/' + SECOND_PARAM + 'data.csv'
+            DATA_DIR =  SEL_DIR + 'DIA_' + data_params.SetDiagnostic(dia) + '__' + data_params.SetSelectionVar(sel) + '_' + var_val + '__SEED_' + seed + '/' + 'data.csv'
 
             # check if data file even exists
             if os.path.isfile(DATA_DIR) == False:
@@ -235,36 +70,30 @@ def DirExplore(data, dump, sel, dia, offs, res, obj, acc, gens, pt):
             df = df.iloc[::res, :]
 
             # time to export the data
-            cdf = pd.DataFrame({'gen': pd.Series(GEN_LIST),
-                            'trt': pd.Series(TRT),
-                            'pop_fit_avg': pd.Series(df[POP_FIT_AVG].tolist()),
-                            'pop_fit_max': pd.Series(df[POP_FIT_MAX].tolist()),
-                            'pop_opt_avg': pd.Series(df[POP_OPT_AVG].tolist()),
-                            'pop_opt_max': pd.Series(df[POP_OPT_MAX].tolist()),
-                            'pop_uni_obj': pd.Series(df[POP_UNI_OBJ].tolist()),
-                            'pop_str_avg': pd.Series(df[POP_STR_AVG].tolist()),
-                            'pop_str_max': pd.Series(df[POP_STR_MAX].tolist()),
-                            'ele_agg_per': pd.Series(df[ELE_AGG_PER].tolist()),
-                            'ele_opt_cnt': pd.Series(df[ELE_OPT_CNT].tolist()),
-                            'arc_acti_gene': pd.Series(df[ARC_ACTI_GENE].tolist()),
-                            # 'opt_agg_per': pd.Series(df[OPT_AGG_PER].tolist()),
-                            # 'opt_obj_cnt': pd.Series(df[OPT_OBJ_CNT].tolist()),
-                            # 'str_agg_per': pd.Series(df[STR_AGG_PER].tolist()),
-                            # 'str_obj_cnt': pd.Series(df[STR_OBJ_CNT].tolist()),
-                            'overlap': pd.Series(df[OVERLAP].tolist()),
-                            'archive_cnt': pd.Series(df[ARCHIVE_CNT].tolist()),
-                            'uni_str_pos':  pd.Series(df[UNI_STR_POS].tolist()),
-                            'pmin':        pd.Series(df[PMIN].tolist()),
-                            'pareto_cnt' :  pd.Series(df[PARETO_CNT].tolist())}) #),
-                            # 'arc_elite':   pd.Series(df[ARC_ELITE].tolist()),
-                            # 'arc_opti':    pd.Series(df[ARC_OPTI].tolist()),
-                            # 'arc_strk':    pd.Series(df[ARC_STRK].tolist())})
-
+            cdf = pd.DataFrame(
+                {   'gen': pd.Series(GEN_LIST),
+                    'trt': pd.Series(TRT),
+                    data_params.POP_FIT_AVG: pd.Series(df[data_params.POP_FIT_AVG].tolist()),
+                    data_params.POP_FIT_MAX: pd.Series(df[data_params.POP_FIT_MAX].tolist()),
+                    data_params.POP_OPT_AVG: pd.Series(df[data_params.POP_OPT_AVG].tolist()),
+                    data_params.POP_OPT_MAX: pd.Series(df[data_params.POP_OPT_MAX].tolist()),
+                    data_params.POP_UNI_OBJ: pd.Series(df[data_params.POP_UNI_OBJ].tolist()),
+                    data_params.POP_STR_AVG: pd.Series(df[data_params.POP_STR_AVG].tolist()),
+                    data_params.POP_STR_MAX: pd.Series(df[data_params.POP_STR_MAX].tolist()),
+                    data_params.ELE_AGG_PER: pd.Series(df[data_params.ELE_AGG_PER].tolist()),
+                    data_params.ELE_OPT_CNT: pd.Series(df[data_params.ELE_OPT_CNT].tolist()),
+                    data_params.ARC_ACTI_GENE: pd.Series(df[data_params.ARC_ACTI_GENE].tolist()),
+                    data_params.OVERLAP: pd.Series(df[data_params.OVERLAP].tolist()),
+                    data_params.ARCHIVE_CNT: pd.Series(df[data_params.ARCHIVE_CNT].tolist()),
+                    data_params.UNI_STR_POS:  pd.Series(df[data_params.UNI_STR_POS].tolist()),
+                    data_params.PMIN:        pd.Series(df[data_params.PMIN].tolist()),
+                    data_params.PARETO_CNT :  pd.Series(df[data_params.PARETO_CNT].tolist())
+                })
             DF_LIST.append(cdf)
 
     fin_df = pd.concat(DF_LIST)
 
-    fin_df.to_csv(path_or_buf= dump + 'al-' + SetDiagnostic(dia).lower() + '-' + gens + '-' + obj + '-' + acc + '.csv', index=False)
+    fin_df.to_csv(path_or_buf= dump + 'over-time-' + data_params.SetDiagnostic(dia).lower() + '-' + gens + '-' + obj + '-' + acc + '.csv', index=False)
 
 def main():
     # Generate and get the arguments
@@ -287,9 +116,9 @@ def main():
     dump_dir = args.dump_dir.strip()
     print('Dump directory=', dump_dir)
     selection = args.selection
-    print('Selection scheme=', SetSelection(selection, args.param_two))
+    print('Selection scheme=', data_params.SetSelection(selection, args.param_two))
     diagnostic = args.diagnostic
-    print('Diagnostic=',SetDiagnostic(diagnostic))
+    print('Diagnostic=', data_params.SetDiagnostic(diagnostic))
     offset = args.seed_offset
     print('Offset=', offset)
     resolution = args.resolution
