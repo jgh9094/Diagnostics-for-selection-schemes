0f9da66f24c49dbbe003f4b739c2e477e212c648
Jose <jgh9094@gmail.com>
2022-11-18 13:05:16 -0500

    file to generate all data for local collection (assuming data files are downloaded from OST)

--- DataTools/setup.R
+++ DataTools/setup.R
@@ -5,11 +5,12 @@ library(dplyr)
 
 TRAITS = 100
 TSIZE = 26
-ORDER = c('Truncation (tru)','Tournament (tor)', 'Genotypic Fitness Sharing (gfs)','Phenotypic Fitness Sharing (pfs)','Lexicase (lex)','Nondominated Sorting (nds)','Novelty Search (nov)','Random (ran)')
-ACRON = tolower(c('TRU','TOR','GFS','PFS','LEX','NDS','NOV','RAN'))
-SHAPE = c(5,3,2,6,1,0,4,20,1)
-PARAM = c('8', '8', '0.3', '0.3', '0.0', '0.3', '15', '1')
-cb_palette <- c('#332288','#88CCEE','#EE3377','#117733','#EE7733','#882255','#44AA99','#CCBB44', '#000000')
+ORDER = c('Truncation (tru)','Tournament (tor)','Lexicase (lex)', 'Genotypic Fitness Sharing (gfs)','Phenotypic Fitness Sharing (pfs)','Nondominated Sorting (nds)','Novelty Search (nov)','Random (ran)')
+ACRON = tolower(c('TRU','TOR','LEX','GFS','PFS','NDS','NOV','RAN'))
+SHAPE = c(5,3,1,2,6,0,4,20,1)
+PARAM = c('8', '8', '0.0', '0.3', '0.3', '0.3', '15', '1')
+cb_palette <- c('#332288','#88CCEE','#EE7733','#EE3377','#117733','#882255','#44AA99','#CCBB44', '#000000')
+GENERATIONS = 50000
 
 # selection scheme parameters
 
@@ -26,10 +27,10 @@ p_theme <- theme(
   plot.title = element_text( face = "bold", size = 22, hjust=0.5),
   panel.border = element_blank(),
   panel.grid.minor = element_blank(),
-  legend.title=element_text(size=18),
-  legend.text=element_text(size=14),
-  axis.title = element_text(size=18),
-  axis.text = element_text(size=16),
+  legend.title=element_text(size=22),
+  legend.text=element_text(size=23),
+  axis.title = element_text(size=23),
+  axis.text = element_text(size=22),
   legend.position="bottom",
   panel.background = element_rect(fill = "#f1f2f5",
                                   colour = "white",
@@ -37,25 +38,33 @@ p_theme <- theme(
 )
 
 # cross comparison data frames
-
 cc_over_time <- read.csv('./DATA-FINAL/POLISHED/cross-comp-over-time.csv', header = TRUE, stringsAsFactors = FALSE)
-colnames(cc_over_time)[19] <- 'Selection\nScheme'
+colnames(cc_over_time)[colnames(cc_over_time) == "Selection.Scheme"] = 'Selection\nScheme'
 cc_over_time$`Selection\nScheme` <- factor(cc_over_time$`Selection\nScheme`, levels = ORDER)
+cc_over_time$uni_str_pos = cc_over_time$uni_str_pos + cc_over_time$arc_acti_gene - cc_over_time$overlap
+
 cc_best = read.csv('./DATA-FINAL/POLISHED/cross-comp-best.csv', header = TRUE, stringsAsFactors = FALSE)
 cc_best$acron <- factor(cc_best$acron, levels = ACRON)
+colnames(cc_best)[colnames(cc_best) == "Selection.Scheme"] = 'Selection\nScheme'
+
 cc_ssf = read.csv('./DATA-FINAL/POLISHED/selection-scheme-ssf.csv', header = TRUE, stringsAsFactors = FALSE)
 cc_ssf$acron <- factor(cc_ssf$acron, levels = ACRON)
+colnames(cc_over_time)[colnames(cc_over_time) == "Selection.Scheme"] = 'Selection\nScheme'
 cc_ssf[is.na(cc_ssf)] <- 59999
+
 cc_end <- filter(cc_over_time, gen == 50000)
 cc_end$acron <- factor(cc_end$acron, levels = ACRON)
 
-
-
 # selection scheme data frames
-
 ss_over_time <- read.csv('./DATA-FINAL/POLISHED/selection-scheme-over-time.csv', header = TRUE, stringsAsFactors = FALSE)
+colnames(ss_over_time)[colnames(ss_over_time) == "Selection.Scheme"] = 'Selection\nScheme'
+ss_over_time$uni_str_pos = ss_over_time$uni_str_pos + ss_over_time$arc_acti_gene - ss_over_time$overlap
+
 ss_best <- read.csv('./DATA-FINAL/POLISHED/selection-scheme-best.csv', header = TRUE, stringsAsFactors = FALSE)
+colnames(ss_best)[colnames(ss_best) == "Selection.Scheme"] = 'Selection\nScheme'
+
 ss_ssf <- read.csv('./DATA-FINAL/POLISHED/selection-scheme-ssf.csv', header = TRUE, stringsAsFactors = FALSE)
+colnames(ss_ssf)[colnames(ss_ssf) == "Selection.Scheme"] = 'Selection\nScheme'
 
 ## genotypic fitness sharing data frames
 gfs_ot <- filter(ss_over_time, acron == 'gfs')
