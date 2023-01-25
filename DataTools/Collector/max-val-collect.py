c23e97751cba1dae3e0f78a6c753f8d8fc5435c3
Jose <jgh9094@gmail.com>
2023-01-25 15:48:23 -0500

    cleaned up and initial implementation

--- DataTools/Collector/max-val-collect.py
+++ DataTools/Collector/max-val-collect.py
@@ -32,80 +32,67 @@ import os
 
 # file location for data-params.py file
 sys.path.insert(1, '../')
-import data_params
+import data_params as dp
 
 # loop through differnt files that exist
-def DirExplore(data, dump, sel, dia, offs, obj, acc, gens, pt, val):
-    # check if data dir exists
-    if os.path.isdir(data) == False:
-        print('DATA=', data)
-        sys.exit('DATA DIRECTORY DOES NOT EXIST')
-
-    # check if data dir exists
-    if os.path.isdir(dump) == False:
-        print('DATA=', data)
-        sys.exit('DATA DIRECTORY DOES NOT EXIST')
+def DirExplore(data, dump, sel, dia, offs, pt, val):
 
     # check that selection data folder exists
-    SEL_DIR = data + data_params.SetSelection(sel,pt) + '/TRT_' + obj + '__ACC_' + acc + '__GEN_' + gens + '/'
-    if os.path.isdir(SEL_DIR) == False:
-        print('SEL_DIR=', SEL_DIR)
-        sys.exit('EXIT -1')
+    # SEL_DIR = dp.GetDataDirectory(dir,sel,pt)
+    SEL_DIR = data
+
+    if val:
+        print('Full data Dir=', SEL_DIR + 'DIA_' + dp.SetDiagnostic(dia) + '__' + dp.SetSelectionVar(sel) + '_XXX' + '__SEED_XXXXXX__MVC/')
+    else:
+        print('Full data Dir=', SEL_DIR + 'DIA_' + dp.SetDiagnostic(dia) + '__' + dp.SetSelectionVar(sel) + '_XXX' + '__SEED_XXXXXX/')
 
     # loop through sub data directories
-    print('Full data Dir=', SEL_DIR + 'DIA_' + data_params.SetDiagnostic(dia) + '__' + data_params.SetSelectionVar(sel) + '_XXX' + '__SEED_XXX' + '/')
+    print('Full data Dir=', SEL_DIR + 'DIA_' + dp.SetDiagnostic(dia) + '__' + dp.SetSelectionVar(sel) + '_XXX' + '__SEED_XXX' + '/')
     print('Now checking data replicates sub directories')
-    VLIST = data_params.SetVarList(sel)
-    SEEDS = data_params.SetSeeds(sel)
 
-    # Will hold the vars
+    # seed set for run
+    SEEDS = dp.SetSeedSets(sel)
+    # model: ea, is, nmis
     TRT = []
+    # best value
     VAL = []
-    COL = []
+    # variable name
+    VAR = []
+    # generation
     GEN = []
+    # selection scheme
+    SEL = []
+    # diagnostic
+    DIA = []
 
-    # data traversing
-    data = {data_params.POP_FIT_AVG, data_params.POP_FIT_MAX, data_params.POP_OPT_AVG,
-            data_params.POP_OPT_MAX, data_params.POP_UNI_OBJ, data_params.POP_STR_AVG,
-            data_params.POP_STR_MAX, data_params.ELE_AGG_PER, data_params.ELE_OPT_CNT,
-            data_params.ARC_ACTI_GENE, data_params.OVERLAP, data_params.ARCHIVE_CNT,
-            data_params.UNI_STR_POS, data_params.PMIN, data_params.POP_MAX_TRT,
-            data_params.POP_MAX_GENE, data_params.PARETO_CNT }
+    for i in range(len(SEEDS)):
+        for s in SEEDS[i]:
+            seed = str(s + offs)
+            DATA_DIR =  SEL_DIR + 'DIA_' + dp.SetDiagnostic(dia) + '__' + dp.SetSelectionVar(sel) + '_' + dp.SetVarList(sel)[i] + '__SEED_' + seed
+            if val:
+                DATA_DIR += '__MVC/data.csv'
+            else:
+                DATA_DIR += '/data.csv'
 
-    for s in SEEDS:
-        seed = str(s + offs)
-        it = int((s-1)/data_params.REPLICATES)
-        var_val = str(VLIST[it])
-        DATA_DIR =  SEL_DIR + 'DIA_' + data_params.SetDiagnostic(dia) + '__' + data_params.SetSelectionVar(sel) + '_' + var_val + '__SEED_' + seed
+            print('Sub directory:', DATA_DIR)
 
-        if val:
-            DATA_DIR += '__MVC/'
-        else:
-            DATA_DIR += '/'        
+            # get data from file and check if can store it
+            df = pd.read_csv(DATA_DIR)
 
-        print('Sub data directory:', DATA_DIR+'data.csv')
+            for key in dp.DATA_LIST:
+                max_val = df[key].max()
+                max_gen = df[df[key] == max_val][dp.GENERATION].values.tolist()[0]
 
-        # get data from file and check if can store it
-        df = pd.read_csv(DATA_DIR+'data.csv')
-
-        for key in data:
-            # Population fit average
-            # Population fit max
-            max_val = df[key].max()
-            max_gen = df[df[key] == max_val][data_params.GENERATION].values.tolist()[0]
-            TRT.append(VLIST[it])
-            VAL.append(max_val)
-            GEN.append(max_gen)
-            COL.append(key)
+                VAL.append(max_val)
+                VAR.append(key)
+                GEN.append(max_gen)
+                SEL.append(dp.SetSelection(sel))
+                DIA.append(dp.SetDiagnostic(i))
+                TRT.append(dp.SetVarList(sel)[i])
 
     # Time to export the csv file
-    # time to export the data
-    fdf = pd.DataFrame({'trt': pd.Series(TRT),
-                    'val': pd.Series(VAL),
-                    'gen': pd.Series(GEN),
-                    'col': pd.Series(COL)})
-
-    fdf.to_csv(path_or_buf= dump + 'best-' + data_params.SetDiagnostic(dia).lower() + '-' + data_params.SetSelection(sel,pt).lower() + '.csv', index=False)
+    fdf = pd.DataFrame({'trt': pd.Series(TRT),'val': pd.Series(VAL),'col': pd.Series(VAR),'gen': pd.Series(GEN),'SEL': pd.Series(SEL),'Diagnostic': pd.Series(DIA)})
+    fdf.to_csv(path_or_buf= dump + 'best-' + dp.SetDiagnostic(dia).lower() + '-' + dp.SetSelection(sel,pt).lower() + '.csv', index=False)
 
 def main():
     # Generate and get the arguments
@@ -115,9 +102,6 @@ def main():
     parser.add_argument("selection",     type=int, help="Selection scheme we are looking for? \n0: (μ,λ)\n1: Tournament\n2: Fitness Sharing\n3: Aggregate Novelty\n4: Espilon Lexicase\n5: Euclidean Novelty")
     parser.add_argument("diagnostic",    type=int, help="Diagnostic we are looking for?\n0: Exploitation\n1: Structured Exploitation\n2: Strong Ecology\n3: Exploration\n4: Weak Ecology")
     parser.add_argument("seed_offset",   type=int, help="Experiment seed offset. (REPLICATION_OFFSET + PROBLEM_SEED_OFFSET")
-    parser.add_argument("objectives",    type=str, help="Number of objectives being optimized")
-    parser.add_argument("accuracy",      type=str, help="Accuracy for experiment")
-    parser.add_argument("generations",   type=str, help="Number of generations experiments ran for")
     parser.add_argument("param_two",     type=str, help="Second paramater for any selection scheme")
     parser.add_argument("--valleys",     type=int, help="True (1) or False (0) on whether or not valleys are applied", action='store', required=False)
 
@@ -128,17 +112,11 @@ def main():
     dump_dir = args.dump_dir.strip()
     print('Dump directory=', dump_dir)
     selection = args.selection
-    print('Selection scheme=', data_params.SetSelection(selection,args.param_two))
+    print('Selection scheme=', dp.SetSelection(selection,args.param_two))
     diagnostic = args.diagnostic
-    print('Diagnostic=', data_params.SetDiagnostic(diagnostic))
+    print('Diagnostic=', dp.SetDiagnostic(diagnostic))
     offset = args.seed_offset
     print('Offset=', offset)
-    objectives = args.objectives
-    print('Objectives=', objectives)
-    accuracy = args.accuracy
-    print('Accuracy=', accuracy)
-    generations = args.generations
-    print('Generations=', generations)
     param_two = args.param_two
     print('2nd param=', param_two)
     valleys = bool(args.valleys)
@@ -146,7 +124,7 @@ def main():
 
     # Get to work!
     print("\nChecking all related data directories now!")
-    DirExplore(data_dir, dump_dir, selection, diagnostic, offset, objectives, accuracy, generations, param_two, valleys)
+    DirExplore(data_dir, dump_dir, selection, diagnostic, offset, param_two, valleys)
 
 if __name__ == "__main__":
     main()
