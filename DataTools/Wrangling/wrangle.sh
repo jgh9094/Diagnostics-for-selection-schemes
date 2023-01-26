1c4c93dbbf2fa1e715064e8cfefc072706a26e36
Jose <jgh9094@gmail.com>
2023-01-26 08:58:20 -0500

    all data gathering calls

--- DataTools/Wrangling/wrangle.sh
+++ DataTools/Wrangling/wrangle.sh
@@ -14,6 +14,15 @@ sbatch solution-fnd.sb
 echo "RUNNING: max-val.sb"
 sbatch max-val.sb
 
+echo "RUNNING: mvc-ot-data.sb"
+sbatch mvc-ot-data.sb
+
+echo "RUNNING: mvc-solution-fnd.sb"
+sbatch mvc-solution-fnd.sb
+
+echo "RUNNING: mvc-max-val.sb"
+sbatch mvc-max-val.sb
+
 echo ""
 echo "======================"
 echo "FINISHED RUNNING FILES"
